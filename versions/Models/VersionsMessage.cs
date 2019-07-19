ef471745c04b6f6117ae7fa7f3b29a153c820afa
Esteban Puentes <esteban.puentes@cern.ch>
2019-07-19 16:40:44 +0200

    Initial Commit

---
+++ versions/Models/VersionsMessage.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json.Linq;
+namespace versions.Models
+{
+    class VersionsMessage
+    {
+        public string Agent { set; get; }
+        public string AutoUpdater { set; get; }
+        public string Plugin { set; get; }
+        public JObject Plugins { set; get; }
+    }
+}
---
+++ versions/Versions.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+
+using plugin;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace versions
+{
+    [PluginAttribute(PluginName = "Versions")]
+    public class IHeartbeat : IInputPlugin
+    {
+        public string Execute(JObject settings)
+        {
+            var versions = new Models.VersionsMessage
+            {
+                Agent = GetAgentVersion(),
+                AutoUpdater = GetAutoUpdaterVersion(),
+                Plugin = GetPluginVersion(),
+                Plugins = GetPluginsVersions()
+            };
+
+            return JsonConvert.SerializeObject(versions);
+        }
+
+        private string GetAgentVersion()
+        {
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(@"winagent.exe");
+
+            return versionInfo.FileVersion;
+        }
+
+        private string GetAutoUpdaterVersion()
+        {
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(@"winagent-updater.exe");
+
+            return versionInfo.FileVersion;
+        }
+
+        private string GetPluginVersion()
+        {
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(@"plugin.dll");
+
+            return versionInfo.FileVersion;
+        }
+
+        private JObject GetPluginsVersions()
+        {
+            var plugins = new JObject();
+            try
+            {
+                foreach (String plugin in Directory.GetFiles("plugins"))
+                {
+                    FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(String.Format(plugin));
+                    plugins[Path.GetFileNameWithoutExtension(plugin)] = versionInfo.FileVersion;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+            return plugins;
+        }
+
+    }
+}
